19dde8c6b1ed71ed37c98feb2cc24abda0a7bc0f
Matt Cassara <cassaram09@gmail.com>
2018-10-10 17:46:08 +0000

    working on captain methods

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.motor_boaters
+    includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+  end
+
+  def self.talented_seafarers
+    where(id: self.motor_boaters.pluck(:id).concat(self.sailors.pluck(:id)) )
+  end
 end
